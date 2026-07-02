4532e83fa62e7943814340d483d7d49ce93748b6
Emmanuel_Rono <ronoemmanuel933@gmail.com>
2026-07-02 13:03:39 +0300

    setup llm

--- src/config.py
+++ src/config.py
@@ -1,12 +1,11 @@
 from pathlib import Path
 import os
-
 from dotenv import load_dotenv
 
 def load_api() -> str:
      env_path = Path(__file__).resolve().parent.parent / ".env"
      load_dotenv(env_path)
-     api_key = os.getenv("OPENAI_API_KEY")
+     api_key = os.getenv("gemini_API_KEY")
      if not api_key:
           raise ValueError("Check api key if present")
      
--- src/main.py
+++ src/main.py
@@ -1,17 +1,17 @@
-from openai import OpenAI
-
+from google import genai
 from config import load_api
 
-api_key = load_api()
-client = OpenAI(api_key = api_key)
 
-response  = client.responses.create(
-    model="gpt-5.5",
+api_key = load_api()
+
+client = genai.Client()
+
+response  = client.interactions.create(
+    model="gemini-3.5-flash",
     input = "Hello, are you set ?"
 
 )
 
-print(response.text)
-
+print(response.output_text)
 
 
